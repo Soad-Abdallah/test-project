9bdcd8f841ea67175d5765224d0cba34546f300c
soad <soadabdallah588@gmail.com>
2021-10-15 20:34:10 +0200

    addition function

--- test/main.cpp
+++ test/main.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+int add_fun(int x, int y)
+{
+
+    int z=x+y;
+    return z;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int num1,num2;
+    cout << "please enter first number : ";
+    cin>>num1;
+
+    cout << "please enter second number : ";
+    cin>>num2;
+    cout<<num1 << "+" << num2 << "=" << add_fun(num1,num2);
     return 0;
 }
